4699720dc665d3ae3c5a12ab459094ec7fb7cacc
Didi <hi@didimedina.com>
2022-12-21 09:16:02 +0200

    initial

--- v4.1/astro.config.mjs
+++ v4.1/astro.config.mjs
@@ -4,20 +4,14 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), image()],
+  integrations: [solidJs(), tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
